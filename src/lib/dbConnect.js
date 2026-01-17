1c0d3f64db55929b9e072b4182712b77e169dd6d
Aditta Chakraborty <cseaditta@gmail.com>
2026-01-17 16:22:29 +0600

    project initialized

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,19 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri = `mongodb+srv://process.env.DB_USERNAME:process.env.DB_PASSWORD@cluster0.gkaujxr.mongodb.net/?appName=Cluster0`;
+const dbName = process.env.DB_USERNAME;
+
+const collections = {
+    PRODUCTS: "products",
+}
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+export const dbConnect = (cName) => {
+    return client.db(dbName).collection(cName);
+}
